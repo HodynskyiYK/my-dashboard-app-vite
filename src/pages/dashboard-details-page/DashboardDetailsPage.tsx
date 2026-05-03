b65fb242ab67de379ff2cae1722dcf36009cf26e
y.hodynskyi <yurii.hodynskyi@rsli.com>
2026-05-03 17:28:35 +0300

    Update DashboardDetailsPage

--- src/pages/dashboard-details-page/DashboardDetailsPage.tsx
+++ src/pages/dashboard-details-page/DashboardDetailsPage.tsx
@@ -1,5 +1,33 @@
+import { useGetDashboardByIdQuery } from "@/entities/dashboards";
+import { Button } from "@/shared/ui/button";
+import { useNavigate, useParams,  } from "react-router-dom";
+
 export function DashboardDetailsPage() {
+    const { id } = useParams();
+    const navigate = useNavigate();
+    const { data: pageDetails, isLoading, error } = useGetDashboardByIdQuery(id || "");
+
+    if (isLoading) {
+        return <div>Loading...</div>;
+    }
+
+    if (error) {
+        return <div>Error loading dashboard details</div>;
+    }
+
+    const createdAt = pageDetails?.createdAt ? new Date(pageDetails.createdAt).toLocaleDateString() : "Unknown date";
+
     return (
-        <h1>Dashboard Details Page</h1>
+        <>
+            <h1>{`Dashboard ${pageDetails?.title}`}</h1>
+            <hr />
+            <p>
+                <em>{`Created at: ${createdAt}`}</em>
+            </p>
+            <hr />
+            <Button type="primary" onClick={() => navigate("/dashboards")}>
+                Back to list
+            </Button>
+        </>
     );
 }
